9bd4a0c6ae57238c25a682cd87acb0ffe2a6ace1
MaryaKaysina <61281550+MaryaKaysina@users.noreply.github.com>
2020-11-10 11:49:26 +0500

    rework php

--- server.php
+++ server.php
@@ -5,12 +5,14 @@ require "./func.php";
 $data = file_get_contents('php://input');
 // file_put_contents('1.json', $data);
 
+if (!isset($data) || empty($data)) die("Ничего не передано");
+
 $json = json_decode($data, true);
 
-$message = "Новая заявка с сайта!";
+$message = "Новая заявка с сайта!\n";
 
 if ($json['step0']['question'] && $json['step0']['answers']) {
-  $message .= $json['step0']['question'] . ": " . implode(", ", $json['step0']['answers']);
+  $message .= "\n" . $json['step0']['question'] . "\nОтвет: " . implode(", ", $json['step0']['answers']);
 } else {
   $response = [
     "status" => "error",
@@ -19,7 +21,7 @@ if ($json['step0']['question'] && $json['step0']['answers']) {
 }
 
 if ($json['step1']['question'] && $json['step1']['answers']) {
-
+  $message .= "\n\n" . $json['step1']['question'] . "\nОтвет: " . implode(", ", $json['step1']['answers']);
 } else {
   $response = [
     "status" => "error",
@@ -28,7 +30,7 @@ if ($json['step1']['question'] && $json['step1']['answers']) {
 }
 
 if ($json['step2']['question'] && $json['step2']['answers']) {
-
+  $message .= "\n\n" . $json['step2']['question'] . "\nОтвет: " . implode(", ", $json['step2']['answers']);
 } else {
   $response = [
     "status" => "error",
@@ -37,7 +39,7 @@ if ($json['step2']['question'] && $json['step2']['answers']) {
 }
 
 if ($json['step3']['question'] && $json['step3']['answers']) {
-
+  $message .= "\n\n" . $json['step3']['question'] . "\nОтвет: " . implode(", ", $json['step3']['answers']);
 } else {
   $response = [
     "status" => "error",
@@ -52,8 +54,15 @@ foreach ($json['step4'] as $item) {
 }
 
 if (!$data_empty) {
+  $message .= "\n\nИмя: " . $json['step4']['name'];
+  $message .= "\nНомер телефона: " . $json['step4']['phone'];
+  $message .= "\nEmail: " . $json['step4']['email'];
+  $message .= "\nСпособ связи:" . $json['step4']['call'];
+  $my_data = [
+    "message" => $message,
+  ];
 
-  get_data(BASE_URL . TOKEN . "/send?message=" . $message);
+  get_data(BASE_URL . TOKEN . "/send?" . http_build_query($my_data));
 
   $response = [
     "status" => "ok",
